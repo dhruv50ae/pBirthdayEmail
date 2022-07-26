43db35506f8f025b992356317b0387622cfe3a4b
dhruv50ae <linkdp2012@gmail.com>
2022-07-26 16:08:27 +0100

    Monday Motivation project

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-# import smtplib
-#
-# # Let it be known that this is a disposable email, you can log in with the given creds if you're quick enough
-# myEmail = "andrew.thatcher.50ae@gmail.com"
-# myPassword = "4qv(KC3bxn6*u("
-#
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=myEmail, password=myPassword)
-#     connection.sendmail(from_addr=myEmail, to_addrs="andrew.thatcher.49ae@gmail.com",
-#                         msg="Subject:Hello\n\nThis is the body of the email")
-
+import smtplib
 import datetime as dt
+import random
+
+MYEMAIL = "andrew.thatcher.50ae@gmail.com"
+MYPASSWORD = "4qv(KC3bxn6*u("
 
 now = dt.datetime.now()
-print(now.year)
+weekday = now.weekday()
+if weekday == 0:
+    with open("quotes.txt") as qFile:
+        allQuotes = qFile.readlines()
+        quote = random.choice(allQuotes)
 
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MYEMAIL, MYPASSWORD)
+        connection.sendmail(from_addr=MYEMAIL, to_addrs=MYEMAIL, msg=f"Subject: Monday Motivation\n\n{quote}")
